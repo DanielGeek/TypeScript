bd0a064d199334d5a7796018ca3cb46b57b1a00a
DanielGeek <daniel30081990@gmail.com>
2019-02-13 02:11:05 -0300

    ejemplo tipos de datos en typescript

---
+++ ejemplo.js
@@ -0,0 +1,18 @@
+// console.log("Hello World");
+var myString = " Hello World";
+myString = 23 + myString;
+var ArrayString = myString.split(" ");
+console.log(ArrayString);
+var myNumber = 22;
+var verdadero = true;
+var cualquierTipo = "hola";
+cualquierTipo = true;
+document.write(myString);
+var ArrayEjemplo = ["", "", ""];
+var ArrayNumeros = [1, 2, 3];
+var arrayBoolea = [true, false, true];
+var arrayAny = [1, "1", " Hola ", false, {}];
+//tuple
+var strNumberTuple;
+strNumberTuple = ["x", 28];
+strNumberTuple = ["x1", 2];
---
+++ ejemplo.ts
@@ -0,0 +1,28 @@
+// console.log("Hello World");
+
+var myString: string = " Hello World";
+
+myString = 23 + myString;
+var ArrayString = myString.split(" ");
+console.log(ArrayString)
+var myNumber: number = 22;
+var verdadero: boolean = true;
+
+var cualquierTipo: any = "hola";
+cualquierTipo = true;
+
+document.write(myString)
+
+var ArrayEjemplo: string[] = ["", "", ""];
+
+var ArrayNumeros: number[] = [1, 2, 3];
+
+var arrayBoolea: boolean[] = [true, false, true];
+
+var arrayAny: any[] = [1, "1", " Hola ", false, {}];
+
+
+//tuple
+var strNumberTuple: [string, number];
+strNumberTuple = ["x", 28];
+strNumberTuple = ["x1", 2];
